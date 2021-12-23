18f9619332a37adaae65b88cfccd406e39433528
davidkc <dakchoy@gmail.com>
2021-12-23 10:00:52 +0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import Form from "./Form"
+import React from "react"
+
+export default function App(){
+    const [formData, setFormData] = React.useState({
+        firstName: "",
+        lastName: ""
+    })
+    function handleChange(e){
+        const {name, value} = e.target
+        setFormData(prevState=>{
+            return {
+                ...prevState,
+                [name]: value
+            }
+
+        })
+    }
+    return(
+        <div>
+            <Form formData={formData} handleChange={handleChange} />
+        </div>
+    )
+}
---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+
+export default function Form(props){
+    const {firstName} = props.formData
+    return(
+        <form>
+            <input 
+                type="text"
+                name="firstName"
+                placeholder="firsName"
+                value={firstName}
+                onChange={props.handleChange}
+            />
+            {firstName}
+        </form>
+    )
+}
